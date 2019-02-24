678ed810c353a25932b8c5e80a2babd1fe46527d
saurabh <saurabhjain.kjsce@gmail.com>
2019-02-24 16:00:11 -0600

    integrated youtube api and axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import SearchBar from "./../components/SearchBar";
-
+import youtube from "./../api/youtube.api";
 class App extends React.Component {
   onSearch = searchValue => {
     console.log(searchValue);
+    youtube.get("/search", {
+      params: { q: searchValue }
+    });
   };
   render() {
     return (
